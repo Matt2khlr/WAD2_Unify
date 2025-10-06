db21ab8bef1836b6e0f9040f8fd5ab4c2e171f46
Matthewkhlr <matthewkhoo.2024@smu.edu.sg>
2025-10-06 10:50:35 +0800

    Added working files
    - html is a folder with basic html documents with minimal VueJs usage
    - Unify has been propagated with a bunch of files which gives me a connection error and a headache i will troubleshoot it some other time

--- Unify/src/App.vue
+++ Unify/src/App.vue
@@ -1,47 +1,72 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+  <Navbar 
+    :pages="pages"
+    :active-page="activePage"
+    :nav-link-click="(index) => activePage = index"
+  ></Navbar>
+  <p>This is the home page</p>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import Navbar from './components/Navbar.vue';
+import PageViewer from './components/PageViewer.vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default {
+  components: {
+    PageViewer,
+    Navbar
+  },
+  data() 
+  {
+    return {
+        activePage: 0,
+        useDarkNavbar: false,
+        pages: [
+            {
+                link: {text:'Home', url:'index.html'},
+                pageTitle: 'Home Page',
+                content: 'This is the home content'
+            },
+            {
+                link: {text:'Study Journal', url:'journal.html'},
+                pageTitle: 'Study Journal',
+                content: 'This is a page to help students record topics they have finished'
+            },
+            {
+                link: {text: 'Well-being tracker', url: 'tracker.html'},
+                pageTitle: 'Well-being Tracker ',
+                content: 'This is a page to help students track their sleep and mental well-being'
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+            },
+            {
+                link: {text: 'Study tips and tricks', url: 'study.html'},
+                pageTitle: 'Study tips and tricks',
+                content: 'This is a page that provides students with tips and tricks to help them study'
+            },
+            {
+                link: {text: 'Nutrition and exercise tracker', url:'nutrition.html'},
+                pageTitle: 'Nutrition and Exercise Tracker',
+                content: 'This is a page that tracks nutrition and exercise.'
+            },
+            {
+                link: {text: 'Calendar', url:'calendar.html'},
+                pageTitle: 'Calendar',
+                content: 'This is a page that allows students to fill in and keep track of their calendar'
+            },
+            {
+                link: {text:'Recommendations', url:'recommendations.html'},
+                pageTitle:'Recommendations',
+                content:'This is a page that allows students to view recommendations throughout the day'
+            }
+        ]
+        
+    };
   }
+    };
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<style>
+/* Optional: custom styles here */
 </style>
